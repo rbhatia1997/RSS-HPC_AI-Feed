527fa3a9f522c97da2d1b9d9d191c6b517a0f346
rbhatia1997 <bhatia.ronak@gmail.com>
2023-09-25 06:10:59 -0500

    feeds now have updated imageURL fixed

--- rss-feeds/src/App.js
+++ rss-feeds/src/App.js
@@ -4,35 +4,48 @@ import './App.css';
 
 const CORS_PROXY = "https://thingproxy.freeboard.io/fetch/";
 const RSS_FEED_URL = "https://aws.amazon.com/blogs/hpc/feed/";
+const FETCH_DELAY = 600000;  // 10 minutes in milliseconds
 
 function App() {
     const [blogData, setBlogData] = useState([]);
 
     useEffect(() => {
-        axios.get(`${CORS_PROXY}${RSS_FEED_URL}`)
-            .then((response) => {
-                const parser = new DOMParser();
-                const xmlDoc = parser.parseFromString(response.data, "text/xml");
-                const items = xmlDoc.getElementsByTagName("item");
-                const blogs = [];
+        const fetchData = () => {
+            axios.get(`${CORS_PROXY}${RSS_FEED_URL}`)
+                .then((response) => {
+                    const parser = new DOMParser();
+                    const xmlDoc = parser.parseFromString(response.data, "text/xml");
+                    const items = xmlDoc.getElementsByTagName("item");
+                    const blogs = [];
 
-                for (let item of items) {
-                    const title = item.getElementsByTagName("title")[0]?.textContent;
-                    const link = item.getElementsByTagName("link")[0]?.textContent;
-                    const description = item.getElementsByTagName("description")[0]?.textContent;
+                    for (let item of items) {
+                        const title = item.getElementsByTagName("title")[0]?.textContent;
+                        const link = item.getElementsByTagName("link")[0]?.textContent;
+                        const description = item.getElementsByTagName("description")[0]?.textContent;
+                        
+                        let imageURL = "";
+                        const encodedContent = item.getElementsByTagName("content:encoded")[0]?.textContent;
+                        if (encodedContent) {
+                            // Parsing the encoded content to extract the image URL
+                            const contentDoc = parser.parseFromString(encodedContent, "text/html");
+                            const imgElement = contentDoc.querySelector('img');
+                            if (imgElement) {
+                                imageURL = imgElement.getAttribute("src") || "";
+                            }
+                        }
 
-                    let imageURL = "";
-                    const imgElement = item.querySelector('img');
-                    if (imgElement) {
-                        imageURL = imgElement.getAttribute("src") || "";
+                        blogs.push({ title, link, description, imageURL });
                     }
 
-                    blogs.push({ title, link, description, imageURL });
-                }
+                    setBlogData(blogs);
+                })
+                .catch((error) => console.error("Error fetching AWS blog content:", error));
+        };
 
-                setBlogData(blogs);
-            })
-            .catch((error) => console.error("Error fetching AWS blog content:", error));
+        fetchData();  // Call immediately on first load
+        const intervalId = setInterval(fetchData, FETCH_DELAY);
+
+        return () => clearInterval(intervalId); // Cleanup when component unmounts
     }, []);
 
     return (
